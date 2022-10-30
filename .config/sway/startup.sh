4217dbdc65fc24131b1697e4dd71bf7d39a30bdd
Bruno Sabença <brunosabenca@users.noreply.github.com>
2022-10-30 17:36:12 +0000

    Fixes startup script

--- .config/sway/startup.sh
+++ .config/sway/startup.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # Load ssh keys
-ssh-add ~/.ssh/id_fourforty 2>&1 > /dev/null < /dev/null
-
-# Automatically lock and turn off screen when idle
-swayidle -w \
-    before-sleep 'swaylockblur'
+ssh-add ~/.ssh/id_fourforty 2>&1 > /dev/null < /dev/null &
 
 # Import Xdefaults
-xrdb -load ~/.Xdefaults
+xrdb -load ~/.Xdefaults &
 
 # Notification manager
 mako &
@@ -19,3 +15,6 @@ nm-applet --indicator &
 
 # Clipboard manager
 wl-paste -t text --watch clipman store --max-items=30 &
+
+# Automatically lock and turn off screen when idle
+swayidle -w before-sleep 'swaylockblur' &
